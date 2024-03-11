632d8f006cf0dfb5747abda86a1e3d1a330938de
Matheus-Alechandre <162439808+Matheus-Alechandre@users.noreply.github.com>
2024-03-11 11:10:45 -0300

    Add files via upload

---
+++ Atividade/calculadora.py
@@ -0,0 +1,10 @@
+num1 = int (input("digite o primeiro número: "))
+num2 = int (input("digite o segundo número: "))
+
+ad = num1 + num2
+sub = num1 - num2 
+div = num1 / num2
+mul = num1 * num2
+
+print (num1, " + ", num2, " = ", ad, "\n", num1, " - ", num2, " = ", sub, "\n", num1," / ", num2, " = ", div, "\n", num1, " x ",
+  num2, " = ", mul)
