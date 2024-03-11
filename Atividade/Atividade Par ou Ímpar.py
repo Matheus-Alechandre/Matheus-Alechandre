519d0ea1b6c4f22fc7868a5308f6d4b7c7fe8509
Matheus-Alechandre <162439808+Matheus-Alechandre@users.noreply.github.com>
2024-03-11 14:35:52 -0300

    Add files via upload

---
+++ Atividade/Atividade Par ou Ímpar.py
@@ -0,0 +1,9 @@
+num = int(input("digite um número e veja se é ímpar ou Par: "))
+
+if num % 2 == 0:
+    print ("\n 0 número inserido é par.")
+
+else:
+    print("\n o número inserido é impar.")
+    
+    
